ff43e0f7602d30e0b873e2aada8079256b555ad5
pnl0cz9p <pnl0cz9p@AHZDL5CG7210LWT.Emea.Royalahold.net>
2020-03-07 21:32:50 +0100

    devops update

---
+++ src/main/java/com/digitalworksolutions/configuration/SwaggerConfig.java
@@ -0,0 +1,34 @@
+package com.digitalworksolutions.configuration;
+
+
+import static springfox.documentation.builders.PathSelectors.regex;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+@Configuration
+@EnableSwagger2
+public class SwaggerConfig {
+	
+	 	@Bean
+	    public Docket productApi() {
+	        return new Docket(DocumentationType.SWAGGER_2)
+	                .select()
+	                .apis(RequestHandlerSelectors.basePackage("com.digitalworksolutions"))
+	                .paths(regex("/citizendetailapi.*"))
+	                .build()
+	                .apiInfo(metaData());
+	    }
+	 	
+	 	
+	    private ApiInfo metaData() {
+	        @SuppressWarnings("deprecation")
+			ApiInfo apiInfo = new ApiInfo("Spring Boot Citizen Details API", "Citizen Details", "1.0", "", "", "", "");
+	        return apiInfo;
+	    }
+
+}
--- src/main/java/com/digitalworksolutions/controller/CitizenDetailsController.java
+++ src/main/java/com/digitalworksolutions/controller/CitizenDetailsController.java
@@ -14,34 +14,42 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
 
 import com.digitalworksolutions.model.CitizenDetails;
 import com.digitalworksolutions.model.CitizenRequest;
 import com.digitalworksolutions.service.CitizenDetailsService;
 import com.digitalworksolutions.util.CustomErrorType;
 
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiResponse;
+import io.swagger.annotations.ApiResponses;
+	
 @RestController
-@RequestMapping("/CitizenDetailAPI/v1")
+@RequestMapping("/citizendetailapi")
+@Api(value="citisen Details Store")
 public class CitizenDetailsController {
 
-	//public static final Logger logger = LoggerFactory.getLogger(CitizenDetailsController.class);
+	// public static final Logger logger =
+	// LoggerFactory.getLogger(CitizenDetailsController.class);
 
 	@Autowired
-	CitizenDetailsService citizenDetailsService; //Service which will do all data retrieval/manipulation work
+	CitizenDetailsService citizenDetailsService; // Service which will do all data retrieval/manipulation work
 
-	// -------------------Retrieve All Users---------------------------------------------
-	@RequestMapping("/")
-	public String welcome() {//Welcome page, non-rest
-		return "Welcome to CitizenDetailsController RestTemplate Example.";
-	}
+	// -------------------Retrieve All
+	// Users---------------------------------------------
+	//@RequestMapping("/")
+	//public String welcome() {// Welcome page, non-rest
+	//return "Welcome to Citizen Details Controller.";
+	//}
 
-	
-	
-		@GetMapping("/citizenList")
-		public ResponseEntity<List<CitizenDetails>> listAllCitizenDetails() {
+	@ApiOperation(value = "View a list of Citizen Details", response = ResponseEntity.class)
+	@ApiResponses(value = { @ApiResponse(code = 200, message = "Successfully retrieved list"),
+			@ApiResponse(code = 401, message = "You are not authorized to view the resource"),
+			@ApiResponse(code = 403, message = "Accessing the resource you were trying to reach is forbidden"),
+			@ApiResponse(code = 404, message = "The resource you were trying to reach is not found") })
+	@RequestMapping(value = "/list", method = RequestMethod.GET, produces = "application/json")
+	public ResponseEntity<List<CitizenDetails>> listAllCitizenDetails() {
 		List<CitizenDetails> citizenDetailsList = citizenDetailsService.findAllCitizenDetails();
 		if (citizenDetailsList.isEmpty()) {
 			return new ResponseEntity<List<CitizenDetails>>(HttpStatus.NO_CONTENT);
@@ -50,47 +58,47 @@ public class CitizenDetailsController {
 		return new ResponseEntity<List<CitizenDetails>>(citizenDetailsList, HttpStatus.OK);
 	}
 
+	// -------------------Retrieve Single citizen
+	// Details------------------------------------------
 	
-	
-	// -------------------Retrieve Single citizen Details------------------------------------------
-		@GetMapping("/citizendetails/{id}")
-		public ResponseEntity<?> getCitizenDetails(@PathVariable("id") long id ) {
-			//logger.info("Fetching User with id {}", id);
-			CitizenDetails  citizenDetails=null;
-			
-		
-			  citizenDetails = citizenDetailsService.findById(id);
-				if (citizenDetails== null) {
-				//logger.error("citizenDetails with id {} not found.", id);
-				return new ResponseEntity<CustomErrorType>(new CustomErrorType("404","citizen Details with id " + id), HttpStatus.NOT_FOUND);
-			}
-			return new ResponseEntity<CitizenDetails>(citizenDetails, HttpStatus.OK);
-		}
+	@ApiOperation(value = "Search a citisen by ID",response = ResponseEntity.class)
+	@RequestMapping(value = "/detail/{id}", method= RequestMethod.GET, produces = "application/json")
+	public ResponseEntity<?> getCitizenDetails(@PathVariable("id") long id) {
+		// logger.info("Fetching User with id {}", id);
+		CitizenDetails citizenDetails = null;
 
-	
-	// -------------------Retrieve Single citizen Details------------------------------------------
-
-	
-	@PostMapping("/citizenfinder")
-	public ResponseEntity<?> getCitizenFinder(@Valid @RequestBody CitizenRequest	citizenRequest) {
-		System.out.println("endter in getCitizenFinder");
-		//logger.info("Fetching User with id {}", id);
-		CitizenDetails  citizenDetails=null;
-		
-		if (citizenRequest!= null) {
-		  citizenDetails = citizenDetailsService.findByNameEmailMoble(citizenRequest);
-		  if(citizenDetails==null) {
-			  return new ResponseEntity<CustomErrorType>(new CustomErrorType("404","citizen through Name Email Mobile "+ citizenRequest.getEmail()+citizenRequest.getMobile() 
-				+ " not found"), HttpStatus.NOT_FOUND);
-		  }
-		}else{
-			//logger.error("citizenDetails with id {} not found.", id);
-			  return new ResponseEntity<CustomErrorType>(new CustomErrorType("404","citizen through Name Email Mobile" + " not found"), HttpStatus.NOT_FOUND);
+		citizenDetails = citizenDetailsService.findById(id);
+		if (citizenDetails == null) {
+			// logger.error("citizenDetails with id {} not found.", id);
+			return new ResponseEntity<CustomErrorType>(new CustomErrorType("404", "citizen Details with id " + id),
+					HttpStatus.NOT_FOUND);
 		}
-		  System.out.println("endter in getCitizenFinder ");
 		return new ResponseEntity<CitizenDetails>(citizenDetails, HttpStatus.OK);
 	}
 
-	
+	// -------------------Retrieve Single citizen
+	// Details------------------------------------------
+
+	@ApiOperation(value = "find citizen by mobile nuber")
+    @RequestMapping(value = "/finder", method = RequestMethod.POST, produces = "application/json")
+	public ResponseEntity<?> getCitizenFinder(@Valid @RequestBody CitizenRequest citizenRequest) {
+		CitizenDetails citizenDetails = null;
+
+		if (citizenRequest != null) {
+			citizenDetails = citizenDetailsService.findByNameEmailMoble(citizenRequest);
+			if (citizenDetails == null) {
+				return new ResponseEntity<CustomErrorType>(
+						new CustomErrorType("404", "citizen through Name Email Mobile " + citizenRequest.getEmail()
+								+ citizenRequest.getMobile() + " not found"),
+						HttpStatus.NOT_FOUND);
+			}
+		} else {
+			// logger.error("citizenDetails with id {} not found.", id);
+			return new ResponseEntity<CustomErrorType>(
+					new CustomErrorType("404", "citizen through Name Email Mobile" + " not found"),
+					HttpStatus.NOT_FOUND);
+		}
+		return new ResponseEntity<CitizenDetails>(citizenDetails, HttpStatus.OK);
+	}
 
 }
